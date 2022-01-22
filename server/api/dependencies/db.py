aed417202c7abf8dcb0deb5575a4548c7a9b40c8
ggarciaxcv <ggarciaxcv@gmail.com>
2022-01-21 16:51:33 -0800

    added get_db_scoped() to return scoped_session

--- server/api/dependencies/db.py
+++ server/api/dependencies/db.py
@@ -1,10 +1,20 @@
 from typing import Generator
-from api.database import SessionLocal
+from api.database import SessionLocal, DbInit
 
 
 def get_db() -> Generator:
     """Yield a SQLAlchemy database session"""
     db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# get scoped SQA session for use with multi-threading in Uploads service
+def get_db_scoped() -> Generator:
+    """Yield a scoped SQLAlchemy database session"""
+    engine = DbInit.new_engine()
+    db = DbInit.new_scoped_session(engine)
     try:
         yield db
     finally:
