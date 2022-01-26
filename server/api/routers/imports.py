ea5cdd7076599a2ad2a2dc100a211c06bd7c6e78
ggarciaxcv <ggarciaxcv@gmail.com>
2022-01-26 14:31:59 -0800

    moved router helper funcs to util submodule

--- server/api/routers/imports.py
+++ server/api/routers/imports.py
@@ -8,11 +8,7 @@
 from fastapi import APIRouter, HTTPException, status, Depends, Form, File, UploadFile
 from sqlalchemy.orm.session import Session
 from starlette.responses import JSONResponse
-from starlette.status import HTTP_401_UNAUTHORIZED
-
-from requests import post
-from os import path, makedirs
-from shutil import copyfileobj
+from starlette import status
 
 from api import schemas
 from api.dependencies.auth import get_current_user
@@ -20,6 +16,7 @@ from api.core.constants import UPLOAD_SVC_ENDPOINT
 from api.core.threads import ExcThread
 from api.crud import UploadCrud
 from api.dependencies.db import get_db
+from api.routers.util.imports import write_file, post_to_upload_service
 
 router = APIRouter(prefix="/api", tags=["prospects_files"])
 
@@ -122,40 +119,3 @@ def new_upload_request(
         },
         201,
     )
-
-
-# write_file writes the given file to the disk cache
-def write_file(file: UploadFile, user_id: int, buffer_size: int):
-    txt = "./cache/{:n}"
-    copy_dir = path.normcase(txt.format(user_id))
-    exists = path.exists(copy_dir)
-    if not exists:
-        makedirs(copy_dir)
-        print("created directory: %s", copy_dir)
-
-    copy_path = path.normcase(copy_dir + "/" + path.basename(file.filename))
-    copy = open(copy_path, "wb")
-    copyfileobj(file.file, copy, buffer_size)
-    copy.close()
-
-    print("file %s copied to cache", copy.name)
-    return copy_path
-
-
-# post user request to upload service
-def post_to_upload_service(endpoint: str, data: schemas.UploadWriteRequest):
-    url = endpoint + "/api/prospects_files/import/upload"
-    response = post(
-        url=url,
-        json={
-            "upload_id": data.upload_id,
-            "file_name": data.file_name,
-            "email_index": data.email_index,
-            "first_name_index": data.first_name_index,
-            "last_name_index": data.last_name_index,
-            "force": data.force,
-            "has_headers": data.has_headers,
-        },
-    )
-    print(response)  # logging only
-    return response
