d6050b323af71f417c1085a6a79827fc335bfc23
ggarciaxcv <ggarciaxcv@gmail.com>
2022-01-25 23:04:20 -0800

    added error responses to get_upload_progress; new_upload_request header index implementation

--- server/api/routers/imports.py
+++ server/api/routers/imports.py
@@ -26,7 +26,9 @@ from api.dependencies.db import get_db
 router = APIRouter(prefix="/api", tags=["prospects_files"])
 
 # get upload progress by checking DB object current state
-@router.get("/prospects_files/{upload_id}/progress", response_model=schemas.UploadStatusResponse)
+@router.get(
+    "/prospects_files/{upload_id}/progress", response_model=schemas.UploadStatusResponse
+)
 def get_upload_progress(
     upload_id: str,
     current_user: schemas.User = Depends(get_current_user),
@@ -34,14 +36,26 @@ def get_upload_progress(
 ):
     """Get upload by id"""
     if not current_user:
-       raise HTTPException(
-           status_code=status.HTTP_401_UNAUTHORIZED, detail="Please log in"
-       )
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Please log in"
+        )
     upload = UploadCrud.get_by_id(db, upload_id)
     if upload is None:
-        return {"total": 0, "done": 0}
+        # requested upload not found
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="The requested Upload was not found.",
+        )
+    if upload.user_id != current_user.id:
+        # current user not authorized to view requested upload
+        # owned by another user
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not authorized to view the requested Upload.",
+        )
     done = upload.created + upload.skipped + upload.updated + upload.failed
     return {"total": upload.total_count, "done": done}
+
 
 # start new file upload
 # actual upload completed by sending upload info
@@ -66,9 +80,9 @@ def new_upload_request(
         file_name = write_file(file, current_user.id, 16384)
         file = open(file_name, "rb")
     except BaseException as err:
-         print(f"write file failed: {err=}, {type(err)=}")
-         raise HTTPException(status_code=500, detail="Something went wrong!")
-         
+        print(f"write file failed: {err=}, {type(err)=}")
+        raise HTTPException(status_code=500, detail="Something went wrong!")
+
     # initialize upload object
     file_info = schemas.UploadFileSource()
     file_info.set_file_info(file, 16384)
@@ -84,13 +98,33 @@ def new_upload_request(
     )
     upload = UploadCrud.create_upload(db, upload_input)
 
-    # send file info to upload service with thread
-    write_data = schemas.UploadWriteRequest(upload_id=upload.id, file_name=upload.file_name, force=force, has_headers=has_headers)
+    # get file header schema & send file info to upload service with thread
+    email_ix, first_ix, last_ix = upload_input.get_header_schema(
+        email_index, first_name_index, last_name_index
+    )
+    write_data = schemas.UploadWriteRequest(
+        upload_id=upload.id,
+        file_name=upload.file_name,
+        email_index=email_ix,
+        first_name_index=first_ix,
+        last_name_index=last_ix,
+        force=force,
+        has_headers=has_headers,
+    )
     t = ExcThread(target=post_to_upload_service, args=(UPLOAD_SVC_ENDPOINT, write_data))
     t.start()
 
     # return success response to user
-    return JSONResponse({"success": True, "message": "Upload iniated!", "file_hash": file_info.get_file_hash(), "id": upload.id}, 201)
+    return JSONResponse(
+        {
+            "success": True,
+            "message": "Upload iniated!",
+            "file_hash": file_info.get_file_hash(),
+            "id": upload.id,
+        },
+        201,
+    )
+
 
 # write_file writes the given file to the disk cache
 def write_file(file: UploadFile, user_id: int, buffer_size: int):
@@ -109,14 +143,21 @@ def write_file(file: UploadFile, user_id: int, buffer_size: int):
     print("file %s copied to cache", copy.name)
     return copy_path
 
+
 # post user request to upload service
 def post_to_upload_service(endpoint: str, data: schemas.UploadWriteRequest):
     url = endpoint + "/api/prospects_files/import/upload"
-    response = post(url=url, json={
-        "upload_id": data.upload_id, 
-        "file_name": data.file_name, 
-        "force": data.force,
-        "has_headers": data.has_headers,
-    })
-    print(response) # logging only
+    response = post(
+        url=url,
+        json={
+            "upload_id": data.upload_id,
+            "file_name": data.file_name,
+            "email_index": data.email_index,
+            "first_name_index": data.first_name_index,
+            "last_name_index": data.last_name_index,
+            "force": data.force,
+            "has_headers": data.has_headers,
+        },
+    )
+    print(response)  # logging only
     return response
