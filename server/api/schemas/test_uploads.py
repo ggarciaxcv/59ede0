1c7eda29ab9cfc49d859b3bf270b862117fe5ead
ggarciaxcv <ggarciaxcv@gmail.com>
2022-01-25 22:55:29 -0800

    get_header_schema class method implementation

--- server/api/schemas/test_uploads.py
+++ server/api/schemas/test_uploads.py
@@ -1,6 +1,6 @@
 import unittest
 
-from api.schemas.uploads import UploadFileSource
+from api.schemas.uploads import UploadFileSource, UploadCreate
 
 # test UploadFileSource class methods
 class TestFileInfo(unittest.TestCase):
@@ -36,8 +36,130 @@ class TestFileInfo(unittest.TestCase):
         self.assertEqual(data.get_file_hash(), "d41d8cd98f00b204e9800998ecf8427e")
         self.assertEqual(data.get_byte_size(), 0)
         self.assertEqual(data.get_line_count(), 0)
-        
 
-if __name__ == '__main__':
+
+class TestGetHeaderSchema(unittest.TestCase):
+    def test_headers_1(self):
+        email_ix = 2
+        first_ix = 0
+        last_ix = 1
+
+        email, first, last = UploadCreate(
+            file_name="", file_hash="", byte_size=0, line_count=0, user_id=0
+        ).get_header_schema(email_ix, first_ix, last_ix)
+        self.assertEqual(email, 2)
+        self.assertEqual(first, 0)
+        self.assertEqual(last, 1)
+
+    def test_headers_2(self):
+        email_ix = 0
+        first_ix = 0
+        last_ix = 0
+
+        email, first, last = UploadCreate(
+            file_name="", file_hash="", byte_size=0, line_count=0, user_id=0
+        ).get_header_schema(email_ix, first_ix, last_ix)
+        self.assertEqual(email, 0)
+        self.assertEqual(first, 1)
+        self.assertEqual(last, 2)
+
+    def test_headers_3(self):
+        email_ix = 0
+        first_ix = 1
+        last_ix = 2
+
+        email, first, last = UploadCreate(
+            file_name="", file_hash="", byte_size=0, line_count=0, user_id=0
+        ).get_header_schema(email_ix, first_ix, last_ix)
+        self.assertEqual(email, 0)
+        self.assertEqual(first, 1)
+        self.assertEqual(last, 2)
+
+    def test_headers_4(self):
+        email_ix = 2
+        first_ix = 0
+        last_ix = 0
+
+        email, first, last = UploadCreate(
+            file_name="", file_hash="", byte_size=0, line_count=0, user_id=0
+        ).get_header_schema(email_ix, first_ix, last_ix)
+        self.assertEqual(email, 2)
+        self.assertEqual(first, 0)
+        self.assertEqual(last, 1)
+
+    def test_headers_5(self):
+        email_ix = 12
+        first_ix = 15
+        last_ix = 25
+
+        email, first, last = UploadCreate(
+            file_name="", file_hash="", byte_size=0, line_count=0, user_id=0
+        ).get_header_schema(email_ix, first_ix, last_ix)
+        self.assertEqual(email, 0)
+        self.assertEqual(first, 1)
+        self.assertEqual(last, 2)
+
+    def test_headers_6(self):
+        email_ix = 0
+        first_ix = 2
+        last_ix = 1
+
+        email, first, last = UploadCreate(
+            file_name="", file_hash="", byte_size=0, line_count=0, user_id=0
+        ).get_header_schema(email_ix, first_ix, last_ix)
+        self.assertEqual(email, 0)
+        self.assertEqual(first, 2)
+        self.assertEqual(last, 1)
+
+    def test_headers_7(self):
+        email_ix = 1
+        first_ix = 0
+        last_ix = 2
+
+        email, first, last = UploadCreate(
+            file_name="", file_hash="", byte_size=0, line_count=0, user_id=0
+        ).get_header_schema(email_ix, first_ix, last_ix)
+        self.assertEqual(email, 1)
+        self.assertEqual(first, 0)
+        self.assertEqual(last, 2)
+
+    def test_headers_8(self):
+        email_ix = 1
+        first_ix = 2
+        last_ix = 0
+
+        email, first, last = UploadCreate(
+            file_name="", file_hash="", byte_size=0, line_count=0, user_id=0
+        ).get_header_schema(email_ix, first_ix, last_ix)
+        self.assertEqual(email, 1)
+        self.assertEqual(first, 2)
+        self.assertEqual(last, 0)
+
+    def test_headers_9(self):
+        email_ix = 0
+        first_ix = 0
+        last_ix = 1
+
+        email, first, last = UploadCreate(
+            file_name="", file_hash="", byte_size=0, line_count=0, user_id=0
+        ).get_header_schema(email_ix, first_ix, last_ix)
+        self.assertEqual(email, 0)
+        self.assertEqual(first, 2)
+        self.assertEqual(last, 1)
+
+    def test_headers_10(self):
+        email_ix = 0
+        first_ix = 1
+        last_ix = 0
+
+        email, first, last = UploadCreate(
+            file_name="", file_hash="", byte_size=0, line_count=0, user_id=0
+        ).get_header_schema(email_ix, first_ix, last_ix)
+        self.assertEqual(email, 0)
+        self.assertEqual(first, 1)
+        self.assertEqual(last, 2)
+
+
+if __name__ == "__main__":
     # begin the unittest.main()
     unittest.main()
--- server/api/schemas/uploads.py
+++ server/api/schemas/uploads.py
@@ -31,6 +31,7 @@ class Upload(BaseModel):
     class Config:
         orm_mode = True
 
+
 # UploadCreate contains default information for intitializing Upload objects in the Uploads db table.
 class UploadCreate(BaseModel):
     file_name: str
@@ -38,6 +39,50 @@ class UploadCreate(BaseModel):
     byte_size: int
     line_count: int
     user_id: int
+
+    # normalize header index values
+    # returns tuple with postitions of email, first, and last name indices
+    @classmethod
+    def get_header_schema(
+        self, email_ix: int, first_ix: int, last_ix: int
+    ) -> (int, int, int):
+        email = 0
+        first = 0
+        last = 0
+
+        ix = {email_ix: "email"}
+        if first_ix in ix:
+            # first_ix == email_ix
+            # (ex: email_ix == 0 with optional values defaulting to 0)
+            if first_ix + 1 == last_ix:
+                # ex: optional last_ix set but optional first_ix is not, defaulting to 0
+                first_ix += 2
+            else:
+                first_ix += 1
+        ix[first_ix] = "first"
+
+        if last_ix in ix:
+            if last_ix + 1 == first_ix:
+                last_ix += 2
+            else:
+                last_ix += 1
+        ix[last_ix] = "last"
+
+        vals = [email_ix, first_ix, last_ix]
+        vals.sort()
+
+        # normalize index values with 0 base ("0, 1, 2")
+        for i, val in enumerate(vals):
+            field = ix[val]
+            if field == "email":
+                email = i
+            if field == "first":
+                first = i
+            if field == "last":
+                last = i
+
+        return email, first, last
+
 
 # UploadCreateRequest represents the POST request data sent to the Import endpoint by the end user.
 class UploadCreateRequest(BaseModel):
@@ -49,22 +94,33 @@ class UploadCreateRequest(BaseModel):
 
     @classmethod
     def as_form(
-        cls, 
+        cls,
         email_index: int = Form(...),
         first_name_index: Optional[int] = Form(...),
         last_name_index: Optional[int] = Form(...),
         force: Optional[bool] = Form(...),
         has_headers: Optional[bool] = Form(...),
-    ) -> 'UploadWriteRequest':
-        return cls(email_index=email_index, first_name_index=first_name_index, last_name_index=last_name_index, force=force, has_headers=has_headers)
+    ) -> "UploadWriteRequest":
+        return cls(
+            email_index=email_index,
+            first_name_index=first_name_index,
+            last_name_index=last_name_index,
+            force=force,
+            has_headers=has_headers,
+        )
+
 
 # UploadWriteRequest contains info sent to the Uploads service
 # by the Import service after a new UploadCreateRequest is made by a user.
 class UploadWriteRequest(BaseModel):
     upload_id: int
     file_name: str
+    email_index: int
+    first_name_index: int
+    last_name_index: int
     force: Optional[bool]
     has_headers: Optional[bool]
+
 
 # UploadCreateResponse represents the data returned to the user after a new upload request is created.
 class UploadCreateResponse(BaseModel):
@@ -73,11 +129,10 @@ class UploadCreateResponse(BaseModel):
     success: bool
     message: str
 
+
 # UploadFileSource contains temporary file data for use while processing new Uploads.
-# Omits pydantic.BaseModel as io.FileIO is not compatible with pydantic validation.
-# Additionally, pydantic is not required as this data structure is only used locally 
-# by the Import service and is not sent across the network.
-class UploadFileSource():
+# Only used locally by the Import service and is not sent across the network.
+class UploadFileSource:
     file_name: str
     file_hash: str
     byte_size: int
@@ -123,6 +178,7 @@ class UploadFileSource():
     @classmethod
     def get_line_count(self) -> int:
         return self.line_count
+
 
 # UploadStatusResponse represents the response for GET requests to /api/prospects_files/:id/progress.
 # 'done' field represents the total number of records created, updated, or skipped.
