3d56e2628a62c42fe94f963b7e4954bd95b81f10
ggarciaxcv <ggarciaxcv@gmail.com>
2022-01-25 23:02:26 -0800

    test header index implementation

--- server/test_main.py
+++ server/test_main.py
@@ -1,8 +1,8 @@
 # test_main.py is used for integration testing the Uploads feature.
-# Tests for the api.routers and api.crud methods were unable to be completed 
-# with the unittest package due the import error raised by the 
+# Tests for the api.routers and api.crud methods were unable to be completed
+# with the unittest package due the import error raised by the
 # package, which is not present when the program runs.
-# This file is ran in conjunction with main.py and a local 
+# This file is ran in conjunction with main.py and a local
 # postgre server.
 # Note: user_id ('1') must be hard coded in /routers/imports.py and r/outers/uploads.py
 # files to run locally.
@@ -17,7 +17,7 @@ from api.routers.imports import post_to_upload_service
 
 # test the /api/prospects_files/import route
 # and /api/prospects_files/import/upload (called by Import route)
-class TestPostToImports():
+class TestPostToImports:
     # without overwrite/update
     def test_post_1(self):
         file = open("./cache/prospects.csv", "rb")
@@ -32,32 +32,45 @@ class TestPostToImports():
         print(response)
         file.close()
 
+
 # test the /api/prospects_files/import/upload route
-class TestPostToUploads():
+class TestPostToUploads:
     def test_post_1(self):
         response = upload_helper("http://0.0.0.0:3001", False)
         print(response)
+
 
 # post simulated user request to import service
 def post_helper(endpoint: str, force: bool, file: FileIO):
     url = endpoint + "/api/prospects_files/import"
     files = {"file": file}
-    payload ={"email_index": 0, "first_name_index": 0, "last_name_index": 0, "force": force, "has_headers": True}
-    response = post(url=url, data=payload, files=files) 
+    payload = {
+        "email_index": 2,
+        "first_name_index": 0,
+        "last_name_index": 0,
+        "force": force,
+        "has_headers": True,
+    }
+    response = post(url=url, data=payload, files=files)
     print(response.text)
     return response
+
 
 # post json request to upload service
 def upload_helper(endpoint: str, force: bool):
     url = endpoint + "/api/prospects_files/import/upload"
-    write_data = schemas.UploadWriteRequest(upload_id=32, file_name="prospects.csv", force=force, has_headers=True)
+    write_data = schemas.UploadWriteRequest(
+        upload_id=32, file_name="prospects.csv", force=force, has_headers=True
+    )
     response = post_to_upload_service(UPLOAD_SVC_ENDPOINT, write_data)
     return response
 
+
 def main():
-   test = TestPostToImports()
-   # test = TestPostToUploads()
-   test.test_post_1()
-   # test.test_post_2()
+    test = TestPostToImports()
+    # test = TestPostToUploads()
+    # test.test_post_1()
+    test.test_post_2()
+
 
 main()
